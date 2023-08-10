7a503cc93b297e97df3e52e075bd276151cbde23
Marsaud olivier <cougies79700@gmail.com>
2023-08-10 15:32:04 +0200

    fix visuel code

--- CréationCompteMrParrot.sql
+++ CréationCompteMrParrot.sql
@@ -1,3 +1,8 @@
 INSERT INTO Roles(role) VALUES ('Administrateur');
 INSERT INTO Roles(role) VALUES ('Employé');
-INSERT INTO Utilisateurs(nom, prenom, mail, mdp, Id_Roles) VALUES ('Parrot', 'Vincent', "garageparot@mail.fr", "012345678901234567890123456789012345678901234567890123456789", 1);
+INSERT INTO Utilisateurs(nom, prenom, mail, mdp, Id_Roles) VALUES (
+    'Parrot',
+    'Vincent',
+    "garageparot@mail.fr",
+    "012345678901234567890123456789012345678901234567890123456789",
+    1);
